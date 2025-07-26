1f839e91f0efc9d43e693f5e1f491708243174e8
Kevin Trinh <trinhke1996@gmail.com>
2025-07-25 22:36:45 -0500

    feat: add README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# tatit2
+# tatit2.com
+
+Custom website for Patchare Ake.
+
+## Workflow
+
+### Technologies
+
+- TailwindCSS and TailwindCSSCli for styles
+- Templ for HTML templating
+- Golang to generate HTML files
+- Taskfile for scripts management
+
+### How To
+
+1. Modify the *.templ files
+2. Run `task generate` to generate CSS, Go, and HTML files
+3. Publish this to GitHub
+4. The GitHub Actions task should bundle and deploy the *.html files
--- main.go
+++ main.go
@@ -13,13 +13,13 @@ var components = map[string]func() templ.Component{
 	"404.html":   NotFound,
 }
 
+// main - generate components and output to *.html files
 func main() {
 	for file, component := range components {
 		f, err := os.Create(file)
 		if err != nil {
 			log.Fatalf("failed to create html file: %v", err)
 		}
-
 		if err = component().Render(context.Background(), f); err != nil {
 			log.Fatalf("failed to write output html file: %v", err)
 		}
